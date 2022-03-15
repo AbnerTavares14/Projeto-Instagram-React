2af6ee22dbc8886aa2b7f64e3819a8482d58f5d4
ElderGr <gbielrs@gmail.com>
2022-03-15 13:04:03 -0300

    Preparando revisão de código

--- src/Imagem.jsx
+++
@@ -1,8 +0,0 @@
-export default function Imagem(props) {
-    const {imagem, id} = props;
-    return (
-                <div key={id} className="imagem">
-                    <img src={imagem} alt="" />
-                </div>
-            )
-}
--- src/Usuario.jsx
+++
@@ -1,8 +0,0 @@
-export default function Usuario(props) {
-    const { usuario } = props;
-    return (
-        <div className="usuario">
-            {usuario}
-        </div>
-    )
-}
